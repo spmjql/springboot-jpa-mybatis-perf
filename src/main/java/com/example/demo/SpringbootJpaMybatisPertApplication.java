de8d28402916a9a9440319fad5ac4ad30e187b34
spmqjl <spmjql@example.com>
2024-03-05 22:42:05 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootJpaMybatisPertApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaMybatisPertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaMybatisPertApplication.class, args);
+	}
+
+}
